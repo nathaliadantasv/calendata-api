d1e5061c994ac8e73d1e1b6cc94b85e2a6dd33e1
Nathalia Dantas Viana <nathaliadantasviana@nathalias-mbp.home>
2025-01-30 20:50:18 +0100

    Initial commit

---
+++ src/main/java/com/nathaliadv/calendataapi/CalenDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.nathaliadv.calendataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathaliadv/calendataapi/CalenDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathaliadv.calendataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
