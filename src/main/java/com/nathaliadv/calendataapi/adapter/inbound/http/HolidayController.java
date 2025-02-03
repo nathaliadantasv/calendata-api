d4ae5f7ed264c855f2ffbad72b56ffabd57816bd
nathaliadantasv <nathaliadantasv@gmail.com>
2025-02-03 09:33:17 +0100

    adding input validations

--- src/main/java/com/nathaliadv/calendataapi/adapter/inbound/http/HolidayController.java
+++ src/main/java/com/nathaliadv/calendataapi/adapter/inbound/http/HolidayController.java
@@ -8,11 +8,16 @@ import com.nathaliadv.calendataapi.core.model.CountryHolidayStatsResponse;
 import com.nathaliadv.calendataapi.core.model.HolidayAnalysisRequest;
 import com.nathaliadv.calendataapi.core.model.HolidayResponse;
 import com.nathaliadv.calendataapi.core.ports.inbound.HolidayInboundPort;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @AllArgsConstructor
 @RestController
 @RequestMapping("/holidays")
@@ -21,23 +26,23 @@ public class HolidayController {
     private final HolidayInboundPort holidayInboundPort;
 
     @GetMapping("/last-three/{countryCode}")
-    public LastHolidaysResponseDTO getLastThreeHolidays(@PathVariable String countryCode) {
+    public LastHolidaysResponseDTO getLastThreeHolidays(@PathVariable @NotBlank @Size(min = 2, max = 3) String countryCode) {
         HolidayAnalysisRequest request = HolidayAnalysisRequest.fromCurrentDate(countryCode);
         List<HolidayResponse> holidays = holidayInboundPort.getTheLastThreeHolidaysCelebrated(request);
         return LastHolidaysResponseDTO.from(holidays);
     }
 
     @GetMapping("/non-weekend")
-    public NonWeekendHolidaysResponseDTO getPublicHolidaysNotOnWeekends(@RequestParam Integer year,
-                                                       @RequestParam List<String> countryCodes) {
+    public NonWeekendHolidaysResponseDTO getPublicHolidaysNotOnWeekends(@RequestParam @NotNull Integer year,
+                                                       @RequestParam @NotNull @Size(min = 1) List<@NotBlank @Size(min = 2, max = 3) String> countryCodes) {
         List<CountryHolidayStatsResponse> holidaysNonWeekend =
                 holidayInboundPort.getNumberOfNonWeekendHolidaysByCountry(countryCodes, year);
         return NonWeekendHolidaysResponseDTO.from(holidaysNonWeekend);
     }
 
     @GetMapping("/common")
-    public CommonHolidaysResponseDTO getCommonHolidays(@RequestParam String firstCountry,
-                                    @RequestParam String secondCountry, @RequestParam Integer year) {
+    public CommonHolidaysResponseDTO getCommonHolidays(@RequestParam @NotBlank @Size(min = 2, max = 3) String firstCountry,
+                                    @RequestParam @NotBlank @Size(min = 2, max = 3) String secondCountry, @RequestParam @NotNull Integer year) {
         CommonHolidaysResponse commonHolidays = holidayInboundPort.getCommonHolidaysBetweenCountries(firstCountry, secondCountry, year);
         return CommonHolidaysResponseDTO.from(commonHolidays);
     }
